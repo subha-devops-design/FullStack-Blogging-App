0595745c548ffc8bf851b61bbc5471122937f3dd
subha-devops-design <dassubhasmita860@gmail.com>
2025-03-26 13:52:18 +0100

    Update variables.tf

--- EKS_Terraform/variables.tf
+++ EKS_Terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = "smp"
+  default     = "smp"  #" as per your keyname to create resource who was availabe to inside your aws"
 }
